5ad5c78c1be6a243db9d929c9ee6397131069556
Dieguez-27 <144158386+Dieguez-27@users.noreply.github.com>
2023-09-06 08:17:27 -0600

    Add files via upload

---
+++ L1-SEDM_1092823.py
@@ -0,0 +1,56 @@
+#Introducciòn a la Programaciòn - seccion 17
+#05/09/2023
+#Sofia Elizabeth Dièguez Martìnez
+#Objetivo: Crear un programa en Python que calcule las 4 operaciones aritmèticas bàsicas
+#Entrada: N1, N2
+"""
+Proceso: 
+Solicitar al usuario 2 nùmeros enteros y realizar los càlculos de sumar, restar, multiplicar, dividir y mostrar los resultados.
+"""
+"""
+Salida:
+Resultado de sumar N1, N2
+Resultado de restar N1, N2
+Resultado de multiplicar N1, N2
+Resultado de dividir N1, N2
+"""
+print("Mi primer programa")
+print("Operación Suma")
+N1 = int(input("Ingrese el número 1: "))
+N2 = int(input("Ingrese el número 2: "))
+
+#Operacion matemática
+Suma= N1+N2
+
+#Resultado
+print("Resultado suma:", Suma)
+
+print("Operación Resta")
+N1 = int(input("Ingrese el número 1: "))
+N2 = int(input("Ingrese el número 2: "))
+
+#Operacion matemática
+Resta= N1-N2
+
+#Resultado
+print("Resultado suma:", Resta)
+
+print("Operación Multiplicación")
+N1 = int(input("Ingrese el número 1: "))
+N2 = int(input("Ingrese el número 2: "))
+
+#Operacion matemática
+Multiplicación= N1*N2
+
+#Resultado
+print("Resultado suma:", Multiplicación)
+
+print("Operación División")
+N1 = int(input("Ingrese el número 1: "))
+N2 = int(input("Ingrese el número 2: "))
+
+#Operacion matemática
+División= N1/N2
+
+#Resultado
+print("Resultado suma:", División)
