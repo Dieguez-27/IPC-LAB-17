54cd792b44a90178a8db47254e616245d0a104e5
Dieguez-27 <144158386+Dieguez-27@users.noreply.github.com>
2023-09-07 09:59:01 -0600

    Add files via upload

---
+++ Ts5-SEDM_1092823.py
@@ -0,0 +1,22 @@
+#Introducción a la programación, sección 17
+#Fecha de creación del programa: 7/09/2023
+#Autor: Sofia Diéguez 
+#Objetivo: Crear un programa que se pueda utilizar para la recoleción de datos.
+#Entrada: Ingresar datos personales 
+"""
+Procesos principales
+1. Solicitarle al usuario su nombre
+2. Solicitarle al usuario su edad
+3. Solicitarle al usuario su carrera
+4. Solicitarle al usuario su carné
+"""
+print("Mi segundo programa")
+#Salida
+sNombre=str(input("Ingrese nombre"))
+sEdad=str(input("Ingrese edad"))
+sCarrera=str(input("Ingrese carrera"))
+sCarne=str(input("Ingrese carné"))
+
+print("Nombre:",sNombre)
+
+print("Soy", sNombre, "tengo", sEdad, "y estudio la carrera de", sCarrera, "Mi número de carné es,", sCarne)
