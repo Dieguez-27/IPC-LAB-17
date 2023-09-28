5b16e97329f34d58bbbbe244f21fd3dc5b9b15cf
Dieguez-27 <144158386+Dieguez-27@users.noreply.github.com>
2023-09-28 09:33:20 -0600

    Add files via upload

---
+++ TS8-SEDM_1092823.py
@@ -0,0 +1,34 @@
+#Introducción a la programación, sección 17
+#Fecha de creación del programa: 28/09/2023
+#Autor: Sofia Diéguez 
+#Objetivo: Crear un programa que pueda realizar una secuencia de números utilizando FOR y WHILE.
+#Entrada: Mostrar secuencia de números
+"""
+Procesos principales
+1. Realizar las secuencias de números requeridas
+2. Mostrar el resultado
+"""
+print("Programa para realizar secuencias de números")
+#Salida
+print("Ejercicio Semana 8: Secuencia de números utilizando FOR y WHILE")
+
+
+for i in range(1, 26):
+    print(i )
+i = 1
+
+while i <= 25:
+    print(i )
+    i += 1
+for i in range(5, 51, 5):
+    print(i )
+i = 5
+while i <= 50:
+    print(i )
+    i += 5
+for i in range(20, -2, -2):
+    print(i )
+i = 20
+while i >= 0:
+    print(i )
+    i -= 2
